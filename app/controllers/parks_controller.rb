6376a5710e4d34bae7a50253e7584aeb5c329c54
Luke Bacon <luke@equivalentideas.com>
2015-06-30 17:22:04 -0700

    Add action and route for a random park

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -2,4 +2,8 @@ class ParksController < ApplicationController
   def show
     @park = Park.find(params[:id])
   end
+
+  def random
+    redirect_to Park.find(Random.new.rand(Park.first.id..Park.last.id))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :parks, only: [:show]
+
+  get 'random' => 'parks#random'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
